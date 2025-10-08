c34cdb82b285766991af642e18bbfc7406f5b39a
Joshua Joseph <75155367+Joshua-Joseph1@users.noreply.github.com>
2025-10-08 16:57:40 +0200

    Update package dependencies and fix favicon path. Upgraded caniuse-lite version and added license information in package-lock.json. Corrected favicon link in layout.tsx to point to the correct directory.

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="icon" href="images/favicon.png" type="image/png" />
       </head>
       <body className={inter.className}>
         <Navbar />
